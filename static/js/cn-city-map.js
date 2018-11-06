071a6700aa51e55cae62a28e75dd739b1acd6298
liaosy <liaosy21@gmail.com>
2018-11-06 18:12:04 +0800

    add new meetup website

---
+++ static/js/cn-city-map.js
@@ -0,0 +1,119 @@
+/**
+ * 百度地图：Elastic Meetup 线下活动城市分布图
+ */
+var geoCoordMap = {
+    "北京":[116.46,39.92],
+    "上海":[121.48,31.22],
+    "广州":[113.23,23.16],
+    "深圳":[114.07,22.62],
+    "杭州":[120.19,30.26],
+    "武汉":[114.31,30.52],
+    "南京":[116.46,39.92],
+    "苏州":[120.37,31.19],
+    "长沙":[113,28.21],
+    "台湾":[121.97,24.08],
+
+
+};
+
+var convertData = function (data) {
+    var res = [];
+    for (var i = 0; i < data.length; i++) {
+        var geoCoord = geoCoordMap[data[i].name];
+        if (geoCoord) {
+            res.push({
+                name: data[i].name,
+                symbol:data[i].symbol,
+                value: geoCoord.concat(data[i].value)
+            });
+        }
+    }
+    return res;
+};
+
+option = {
+
+    title: {
+        text: 'Elastic Meetup 线下活动城市分布图',
+        subtext: '',
+        sublink: '',
+        x:'center',
+        textStyle: {
+            color: '#000000'
+        }
+    },
+    tooltip: {
+        trigger: 'item',
+        formatter: function (params) {
+            return params.name + '' + params.value[2];
+        }
+    },
+    legend: {
+        orient: 'vertical',
+        y: 'bottom',
+        x:'right',
+        data:['pm2.5'],
+        textStyle: {
+            color: '#fff'
+        }
+    },
+    geo: {
+        map: 'china',
+        label: {
+            emphasis: {
+                show: false
+            }
+        },
+        itemstyle: {
+            normal: {
+
+                borderColor: '#111'
+            },
+            emphasis: {
+                areaColor: 'yellow'
+            }
+        }
+    },
+    series: [
+        {
+            name: '活动分布图',
+            type: 'scatter',
+            coordinateSystem: 'geo',
+            data: convertData([
+                {name: "北京",symbol:'pin',value: ""},
+                {name: "上海",symbol:'pin',value: ""},
+                {name: "广州",symbol:'pin',value: ""},
+                {name: "深圳",symbol:'pin',value: ""},
+                {name: "杭州",symbol:'pin',value: ""},
+                {name: "武汉",symbol:'pin',value: ""},
+                {name: "南京",symbol:'pin',value: ""},
+                {name: "苏州",symbol:'pin',value: ""},
+                {name: "长沙",symbol:'pin',value: ""},
+                {name: "台湾",symbol:'pin',value: ""},
+            ]),
+            roam: true,
+            symbolSize: 50,
+            label: {
+                normal: {
+                    show: true,
+                    formatter: '{b}'
+                },
+                emphasis: {
+                    show: false
+                }
+            },
+            itemsstyle: {
+                emphasis: {
+                    borderColor: '#fff',
+                    borderWidth: 1
+                }
+            }
+        }
+    ]
+}
+
+$.get('/static/plugins/echarts/map/china.json', function (chinaJson) {
+    echarts.registerMap('china', chinaJson);
+    var chart = echarts.init(document.getElementById('city-map'));
+    chart.setOption(option);
+});
---
+++ static/js/map.js
@@ -0,0 +1,18 @@
+/**
+ * 百度地图 设置定位
+ */
+var map = new BMap.Map("mapContainer");
+var geoc = new BMap.Geocoder();
+
+//获取地址
+var mapAddressCity = document.getElementsByClassName("map-address-city")[0].innerText;
+var mapAddressDes = document.getElementsByClassName("map-address-des")[0].innerText;
+geoc.getPoint(mapAddressDes, function (point) {
+    if (point) {
+        map.clearOverlays();
+        var marker = new BMap.Marker(point);
+        map.centerAndZoom(point, 16);
+        map.addOverlay(marker);
+        marker.setAnimation(BMAP_ANIMATION_BOUNCE);
+    }
+}, mapAddressCity);
